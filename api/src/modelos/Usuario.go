44acb0aef02fba68126ffeed313ba2278a57bdf4
HigorSouza <higor.andrade@hotmail.com>
2022-12-13 14:28:36 -0300

    Tratamento de erros e controle de respostas das requisições

--- api/src/modelos/Usuario.go
+++ api/src/modelos/Usuario.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Usuario representa um usuário utilizando a rede social
 type Usuario struct {
@@ -10,4 +14,40 @@ type Usuario struct {
 	Email    string `json:"Email,omitempty"`
 	Senha    string `json:"Senha,omitempty"`
 	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+}
+
+// Preparar vai chamar os métodos para validar e formatar o usuário recebido
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (Usuario *Usuario) validar() error {
+	if Usuario.Nome == "" {
+		return errors.New("O nome é obrigatório e não pode estar em banco.")
+	}
+
+	if Usuario.Nick == "" {
+		return errors.New("O nick é obrigatório e não pode estar em banco.")
+	}
+
+	if Usuario.Email == "" {
+		return errors.New("O email é obrigatório e não pode estar em banco.")
+	}
+
+	if Usuario.Senha == "" {
+		return errors.New("A senha é obrigatório e não pode estar em banco.")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }
